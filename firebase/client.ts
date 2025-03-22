84faaa6fece23c018ea7cdb0f946ef7ad82594ab
John <johnmuller0910@gmail.com>
2025-03-22 17:35:09 +0300

    implemented auth, storage and user auth

---
+++ firebase/client.ts
@@ -0,0 +1,20 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5eTiP7PsWXAAmqPscFORrxTRfMw5SiNE",
+  authDomain: "prepwise-f504f.firebaseapp.com",
+  projectId: "prepwise-f504f",
+  storageBucket: "prepwise-f504f.firebasestorage.app",
+  messagingSenderId: "424786356027",
+  appId: "1:424786356027:web:2ef79edaeef3867da8b91e",
+  measurementId: "G-6SJ44V2HYD",
+};
+//12433
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
